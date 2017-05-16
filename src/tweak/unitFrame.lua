b46e9bf4a92d3fa88c9e136bb056cb73f6bc15a6
tanghao <tanghao.hb@gmail.com>
2017-05-16 09:43:25 +0800

    refine unitFrame a bit

--- src/tweak/unitFrame.lua
+++ src/tweak/unitFrame.lua
@@ -3,21 +3,24 @@ T.ask("api").answer("addTargetFrameClassIcon", function(api)
     button:SetPoint("topleft", 115, -3); -- no pixel fix since relate to bliz mod
     RaiseFrameLevel(button);
     artTexture:SetTexture([[Interface\WorldStateFrame\Icons-Classes]]);
+    button.artTexture = artTexture;
 
     button:SetScript("OnMouseDown", function(self, button)
         if not UnitCanAttack("player", "target") and UnitIsPlayer("target") then
-            if InspectFrame and InspectFrame:IsShown() then
-                InspectFrame:Hide();
-            else
-                InspectUnit("target");
+            if button == "LeftButton" then
+                if InspectFrame and InspectFrame:IsShown() then
+                    InspectFrame:Hide();
+                else
+                    InspectUnit("target");
+                end
             end
         end
     end);
 
-    local function updateClassIcon(unit, button, artTexture)
+    local function updateClassIcon(unit, button)
         if UnitIsPlayer(unit) then
             local unitClass = select(2, UnitClass(unit));
-            artTexture:SetTexCoord(unpack(CLASS_ICON_TCOORDS[unitClass]));
+            button.artTexture:SetTexCoord(unpack(CLASS_ICON_TCOORDS[unitClass]));
             button:Show();
         else
             button:Hide();
@@ -26,10 +29,10 @@ T.ask("api").answer("addTargetFrameClassIcon", function(api)
 
     button:RegisterEvent("PLAYER_TARGET_CHANGED");
     button:SetScript("OnEvent", function(self, event, ...)
-        updateClassIcon("target", button, artTexture);
+        updateClassIcon("target", button);
     end);
 
-    updateClassIcon("target", button, artTexture);
+    updateClassIcon("target", button);
 end);
 
 -- TODO duplicate unit casting bar over its hp bar or name line
