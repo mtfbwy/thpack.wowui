4e769dcb1b39d4181462742dd752780202df3988
tanghao <tanghao.hb@gmail.com>
2019-09-05 07:57:50 +0800

    merge build

--- build/build.sh
+++ build/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-ADDONS="/mnt/d/app/World of Warcraft/_retail_/Interface/AddOns"
-
-if test ! -d "$ADDONS"; then
-    echo "E: invalid wow addon dir"
+WOW="/mnt/d/app/World of Warcraft"
+if test ! -d "$WOW"; then
+    echo "E: invalid wow dir"
     exit
 fi
 
-SELF=thpack.wowui
-DST="$ADDONS/$SELF";
+ADDON="$WOW/_retail_/Interface/AddOns/thpack.wowui"
+rm -rf "$ADDON"
+mkdir -p "$ADDON"
+cp *.toc *.xml -r res src "$ADDON"
 
-rm -rf "$DST"
-mkdir -p "$DST"
-
-cp $SELF.toc bindings.xml -r res src "$DST"
+ADDON="$WOW/_classic_/Interface/AddOns/thpack.classic"
+rm -rf "$ADDON"
+mkdir -p "$ADDON"
+cp *.toc *.xml -r res src "$ADDON"
