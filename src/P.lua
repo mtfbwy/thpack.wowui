1d4aaa58174e1260f8d55bc15855669b1e931c73
tanghao <tanghao.hb@gmail.com>
2019-09-04 11:51:23 +0800

    rewrite P, basically remove dependency count

--- src/P.lua
+++ src/P.lua
@@ -1,154 +1,152 @@
---------------------
--- framework
+_G["P"] = (function(ADDON)
 
-_G["P"] = (function(NAME)
-
-    local VERSION = 875;
-
-    local dummy = function()
-        -- dummy
-    end;
-
-    local genName = (function()
-        local i = -1;
+    local generateModName = (function()
+        local nameIndex = -1;
         return function()
-            i = i + 1;
-            return "noname-" .. i;
+            nameIndex = nameIndex + 1;
+            return "noname-" .. nameIndex;
         end;
     end)();
 
-    local store = Store:create();
+    local modStore = Store:create();
 
-    local accept = (function()
+    local mayReadyQueue = {};
 
-        local readys = {};
+    local dependencyMap = {
+        -- blockerName = [ name, name, ... ]
+    };
 
-        local blockers = {
-            -- blockerName = [ name, name, ... ]
-        };
+    local isExecuting = false;
 
-        local isWorking = false;
+    local execute = function()
+        if (isExecuting) then
+            return;
+        end
 
-        function work()
-            if (isWorking) then
+        isExecuting = true;
+
+        if (#mayReadyQueue == 0) then
+            isExecuting = false;
+            return;
+        end
+
+        local mod = table.remove(mayReadyQueue, 1);
+        if (mod.statusCode == 200) then
+            isExecuting = false;
+            return;
+        end
+
+        -- verify upstream mods all executed and prepare args
+        local blockerModResults = {};
+        for i = 1, #mod.upstreamModNames do
+            local blockerMod = modStore:get(mod.upstreamModNames[i]);
+            if (blockerMod == nil or blockerMod.statusCode ~= 200) then
+                isExecuting = false;
                 return;
             end
-            if (#readys == 0) then
-                return;
-            end
-            isWorking = true;
-
-            local o = table.remove(readys, 1);
-            local blockerResults = {};
-            for i = 1, #o.blockerNames do
-                local blocker = store:get(o.blockerNames[i]);
-                blockerResults[i] = blocker.result;
-            end
-            o.result = o.fn(unpack(blockerResults)) or {};
-
-            for i, blockedName in pairs(blockers[o.name] or {}) do
-                local blocked = store:get(blockedName);
-                blocked.p = blocked.p - 1;
-                if (blocked.p == 0) then
-                    markReady(blocked);
-                end
-            end
-            blockers[o.name] = nil;
-
-            isWorking = false;
+            blockerModResults[i] = blockerMod.result;
         end
 
-        local workTrigger = Timer:create()
-            :schedule(work, 25, 1000)
-            :stop();
+        if (type(mod.fn) == "function") then
+            mod.result = mod.fn(unpack(blockerModResults));
+        end
+        mod.statusCode = 200;
 
-        local workThrottler = Timer:create()
-            :schedule(function()
-                    workTrigger:stop();
-                end, 4000, 1)
-            :stop();
-
-        function markReady(o)
-            table.insert(readys, o);
-            if (not workThrottler:isRunning()) then
-                workTrigger:reschedule();
-                workThrottler:reschedule();
-            else
-                workThrottler:reschedule();
-            end
+        -- notify downstream mods
+        for i, blockedModName in pairs(dependencyMap[mod.name] or {}) do
+            mayReady(modStore:get(blockedModName));
         end
 
-        function accept(name)
-            local o = store:get(name);
-            for i = 1, #o.blockerNames do
-                local blockerName = o.blockerNames[i];
-                local blocker = store:get(blockerName);
-                if (blocker == nil or blocker.result == nil) then
-                    if (blockers[blockerName] == nil) then
-                        blockers[blockerName] = {};
-                    end
-                    table.insert(blockers[blockerName], o.name);
-                    o.p = o.p + 1;
-                end
-            end
-            if (o.p == 0) then
-                markReady(o);
-            end
+        isExecuting = false;
+    end;
+
+    local workTrigger = Timer:create()
+        :schedule(execute, 25, 1000)
+        :stop();
+
+    local workThrottler = Timer:create()
+        :schedule(function()
+                workTrigger:stop();
+            end, 4000, 1)
+        :stop();
+
+    local mayReady = function(mod)
+        table.insert(mayReadyQueue, mod);
+        if (not workThrottler:isRunning()) then
+            workTrigger:reschedule();
+            workThrottler:reschedule();
+        else
+            workThrottler:reschedule();
+        end
+    end;
+
+    function accept(modName, modFn, upstreamModNames)
+        if (modStore:contains(name)) then
+            error(string.format("E: name conflict: [%s]", name));
+            return;
         end
 
-        Timer:create():schedule(function()
-            local isLogging = false;
-            Timer:create():schedule(function()
-                local blockerKeys = table.keys(blockers);
-                local blockerSize = #blockerKeys;
-                if (blockerSize > 0 or isLogging) then
-                    logi(string.format("W: [%s] blockers remaining: %s", blockerSize, table.concat(blockerKeys, ", ")));
-                end
-                local readysSize = #readys;
-                if (readysSize > 0 or isLogging) then
-                    logi("W: [" .. readysSize .. "] readys remaining");
-                end
-                isLogging = blockerSize > 0 or readysSize > 0;
-            end, 1000, 12);
-        end, 4000, 1);
+        modStore:put(modName, {
+            upstreamModNames = upstreamModNames or {},
+            name = modName,
+            fn = modFn,
+            statusCode = 0, -- 0:created,200:OK,400:error
+            result = nil
+        });
 
-        return accept;
-    end)();
+        local mod = modStore:get(name);
 
-    function ask(...)
+        for i = 1, #mod.upstreamModNames do
+            local blockerModName = mod.upstreamModNames[i];
+            if (dependencyMap[blockerModName] == nil) then
+                dependencyMap[blockerModName] = {};
+            end
+            table.insert(dependencyMap[blockerModName], mod.name);
+        end
+        mayReady(mod);
+    end
+
+    local ask = function(...)
         local a = {...};
         return {
             answer = function(name, fn)
                 if (name == nil) then
-                    name = genName();
+                    name = generateModName(); -- for debugging
                 elseif (type(name) ~= "string") then
-                    error(string.format("E: invalid argument: String expected"));
-                    return;
-                elseif (store:contains(name)) then
-                    error(string.format("E: name conflict: [%s] already exists", name));
+                    error(string.format("E: invalid argument: string expected"));
                     return;
                 end
-                if (fn == nil) then
-                    fn = dummy;
-                elseif (type(fn) ~= "function") then
+
+                if (fn ~= nil and type(fn) ~= "function") then
                     error(string.format("E: invalid argument: function expected"));
                     return;
                 end
-                store:put(name, {
-                    blockerNames = a or {},
-                    name = name,
-                    fn = fn,
-                    p = 0,
-                    result = nil
-                });
-                accept(name);
+
+                accept(name, fn, a);
             end
         };
     end;
 
+    -- tracking
+    Timer:create():schedule(function()
+        Timer:create():schedule(function()
+            local blockedModNames = {};
+            local modNames = table.keys(dependencyMap);
+            for i = 1, #modNames do
+                local mod = modStore:get(modNames[i]);
+                if (mod.statusCode ~= 200) then
+                    table.insert(blockedModNames, mod.name);
+                end
+            end
+            if (#blockedModNames > 0) then
+                logi(string.format("W: Not executed: %s", table.concat(blockedModNames, ", ")));
+            end
+        end, 1000, 12);
+    end, 4000, 1);
+
     return {
-        _name = NAME,
-        _version = VERSION,
+        _addon = ADDON,
+        _version = 875,
         ask = ask
     };
 end)(...);
