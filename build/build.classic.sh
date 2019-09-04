0f4293d4f5c2cdbef221034e8d8b9d5cf53d6dda
tanghao <tanghao.hb@gmail.com>
2019-09-04 08:19:58 +0800

    add classic

--- build/build.classic.sh
+++ build/build.classic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ADDONS="/mnt/d/app/World of Warcraft/_retail_/Interface/AddOns"
+ADDONS="/mnt/d/app/World of Warcraft/_classic_/Interface/AddOns"
 
 if test ! -d "$ADDONS"; then
     echo "E: invalid wow addon dir"
     exit
 fi
 
-SELF=thpack.wowui
+SELF="thpack.classic"
 DST="$ADDONS/$SELF";
 
 rm -rf "$DST"
