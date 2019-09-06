a66b992ce2bc5a951afbcf54a77f9b757d13430c
tanghao <tanghao.hb@gmail.com>
2019-09-06 22:21:13 +0800

    add moveUpErrorsFrame to classic

---
+++ src/classic/moveUpErrorsFrame.lua
@@ -0,0 +1,4 @@
+(function()
+    UIErrorsFrame:ClearAllPoints();
+    UIErrorsFrame:SetPoint("TOP", 0, -30);
+end)();
--- src/tweak/tweakErrorsFrame.lua
+++
@@ -1,4 +0,0 @@
-P.ask("pp").answer("tweakErrorsFrame", function(pp)
-    UIErrorsFrame:ClearAllPoints();
-    UIErrorsFrame:SetPoint("TOP", 0, -40 * pp.dp);
-end);
