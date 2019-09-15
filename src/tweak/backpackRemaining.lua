7f272966b54976cbf70a5cb0667f96aae3297f59
tanghao <tanghao.hb@gmail.com>
2019-09-15 08:47:42 +0800

    add backpack empty slot

---
+++ src/tweak/backpackRemaining.lua
@@ -0,0 +1,19 @@
+(function()
+    local f = CreateFrame("frame");
+    f:RegisterEvent("BAG_UPDATE");
+    f:SetScript("OnEvent", function(self, event, ...)
+        local bagId = (...);
+        -- check FrameXML/Constants.lua
+        -- BACKPACK_CONTAINER = 0
+        -- NUM_BAG_SLOTS = 4
+        if (bagId >= 0 and bagId <= 4) then
+            -- check FrameXML/MainMenuBarBagButtons.lua
+            -- check FrameXML/MainMenuBarBagButtons.xml
+            -- check FrameXML/ItemButtonTemplate.xml
+            if MainMenuBarBackpackButtonCount then
+               local n = CalculateTotalNumberOfFreeBagSlots();
+               MainMenuBarBackpackButtonCount:SetText(string.format("(%d)", n));
+            end
+        end
+    end);
+end)();
