233b850951d996bff255ee59de591cc5f557e925
tanghao <tanghao.hb@gmail.com>
2019-09-15 21:06:22 +0800

    set back unit tooltip info

--- src/tweak/tweakUnitTooltip.lua
+++ src/tweak/tweakUnitTooltip.lua
@@ -1,40 +1,32 @@
 (function()
-
-    function getUnitString(unit)
-        if (not unit or not UnitExists(unit)) then
-            return "";
-        end
-
-        local unitName = UnitName(unit);
-        local hostileColor = Addon.Color.fromUnitHostile(unit);
-
-        if (UnitIsPlayer(unit)) then
-            local classColor = Addon.Color.fromUnitClass(unit);
-            return string.format("|cff%06x%s|r|cff%06x%s|r",
-                    Addon.Color.toInt24(hostileColor), "@",
-                    Addon.Color.toInt24(classColor), unitName);
-        else
-            return string.format("|cff%06x%s|r",
-                    Addon.Color.toInt24(hostileColor), unitName);
-        end
-    end
-
     GameTooltip:HookScript("OnTooltipSetUnit", function(self)
         local _, unit = self:GetUnit();
         if not unit then
             return;
         end
 
-        local unitString = getUnitString(unit);
-
-        local unitTargetString;
-        local unitTarget = unit .. "target";
-        if (UnitIsUnit(unitTarget, "player")) then
-            unitTargetString = "|cffff0000!!!|r";
-        else
-            unitTargetString = getUnitString(unitTarget);
+        local unitString = "";
+        if (UnitIsPlayer(unit)) then
+            local classColor = Addon.Color.fromUnitClass(unit);
+            unitString = string.format("|cff%06x%s|r",
+                    Addon.Color.toInt24(classColor), UnitClass(unit));
         end
 
-        self:AddDoubleLine(unitString, "→ " .. unitTargetString, 1, 1, 1);
+        local unitTargetString = nil;
+        local unitTarget = unit .. "target";
+        if (UnitExists(unitTarget)) then
+            if (UnitIsUnit(unitTarget, "player")) then
+                unitTargetString = "|cffff0000!!!|r";
+            elseif (UnitIsPlayer(unitTarget)) then
+                local hostileColor = Addon.Color.fromUnitHostile(unitTarget);
+                unitTargetString = string.format("|cff%06x%s|r",
+                        Addon.Color.toInt24(hostileColor),
+                        UnitName(unitTarget));
+            end
+        end
+        if (unitTargetString) then
+            unitTargetString = "→ " .. unitTargetString;
+        end
+        self:AddDoubleLine(unitString, unitTargetString, 1, 1, 1, 1, 1, 1);
     end);
 end)();
