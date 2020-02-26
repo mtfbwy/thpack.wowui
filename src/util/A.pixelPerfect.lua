2f0a9c18fdbf5ba7a19dabfaf001d9efabb9e196
tanghao <tanghao.hb@gmail.com>
2020-02-26 22:23:08 +0800

    PixelPerfect: support DISPLAY_SIZE_CHANGED

--- src/util/A.pixelPerfect.lua
+++ src/util/A.pixelPerfect.lua
@@ -1,87 +1,95 @@
 -- pixel perfect
 (function()
 
-    if (CONFIG == nil) then
-        CONFIG = {};
-    end
+    CONFIG = CONFIG or {};
 
-    local A = A or {
-        -- add fake dependencies in case
-        addSlashCommand = function() end,
-        logi = function() end,
-    };
-
-    -- extra command for user to force his screen resolution
-    A.addSlashCommand("thpackScreenResolution", "/screenResolution", function(x)
+    -- extra command for user to force his client resolution
+    -- this is necessary for those mods using A.px and A.dp
+    A.addSlashCommand("thpackClientResolution", "/resolution", function(x)
         if (x == nil or x == "") then
-            A.logi("Pixel-perfect depends on screen resolution.");
-            A.logi("  e.g. /screenResolution reset");
-            A.logi("  e.g. /screenResolution 1024x768");
+            A.logi("Pixel-perfect depends on correct resolution.");
+            A.logi("  e.g. /resolution reset");
+            A.logi("  e.g. /resolution 1024x768");
         elseif (x == "unset" or x == "reset" or x == "clear" or x == "nil") then
-            CONFIG.screenResolution = nil;
-            A.logi("Screen resolution is reset. The change will take effect after reload.");
+            CONFIG.clientResolution = nil;
+            A.logi("Resolution is reset. Reload to apply.");
         else
-            CONFIG.screenResolution = x;
-            A.logi(string.format("Screen resolution [%s] saved. The change will take effect after reload.", x));
+            CONFIG.clientResolution = x;
+            A.logi(string.format("Resolution [%s] is saved. Reload to apply.", x));
         end
     end);
 
-    local screenResolution;
-    if (CONFIG.screenResolution ~= nil) then
-        screenResolution = CONFIG.screenResolution;
-        A.logi(string.format("pixel-perfect: [%s] loaded. (see %s)", screenResolution, "/screenResolution"));
-    else
-        -- once upon a time screenResolution is GetCVar("gxResolution")
-        local possibleResolutions = { GetScreenResolutions() };
-        local resolutionIndex = GetCurrentResolution();
-        if (resolutionIndex == nil or resolutionIndex < 1) then
-            resolutionIndex = 1;
+    local function getSavedClientResolution()
+        if (CONFIG and CONFIG.clientResolution) then
+            local clientResolution = CONFIG.clientResolution;
+            A.logi(string.format("Resolution [%s] loaded. (see %s)", clientResolution, "/resolution"));
+            return clientResolution;
         end
-        screenResolution = possibleResolutions[resolutionIndex];
-        A.logi(string.format("pixel-perfect: [%s] detected. (see %s)", screenResolution, "/screenResolution"));
     end
 
-    local yResolution = tonumber(string.match(screenResolution, "%d+x(%d+)"));
-    if (yResolution < 768) then
-        A.logi(string.format("pixel-perfect: Y-Resolution has min value 768. [%s] ignored.", screenResolution));
-        yResolution = 768;
+    local function getClientResolution()
+        -- once upon a time the resolution is GetCVar("gxResolution")
+        local clientResolution;
+        local index = GetCurrentResolution();
+        if (index and index > 0) then
+            clientResolution = select(index, GetScreenResolutions());
+        else
+            clientResolution = GetCVar("gxWindowedResolution");
+        end
+        A.logi(string.format("Resolution [%s] detected. (see %s)", clientResolution, "/resolution"));
+        return clientResolution;
     end
 
-    -- point is the unit used by Blizzard UI
-    -- canvas has default height 768 (point) and forced to keep aspect ratio
-    --      canvasHeight = 768 (point) / scale
-    --      screenHeight = yResolution (pixel)
-    --      =>  1 (pixel/point) = 768 / scale / yResolution
-    -- scale can be uiScale or UIParent:GetEffectiveScale()
-    -- uiScale must in [0.64, 1]
-    -- effectiveScale has no limit but will not saved in config
+    local function getClientHeight(clientResolution)
+        return tonumber(string.match(clientResolution, "%d+x(%d+)"));
+    end
 
-    -- prefer effectiveScale to uiScale
-    SetCVar("useUiScale", 0);
-
-    -- keep a point integer multiple of pixel
-    local numPixelsPerPoint = math.floor(yResolution / 768 + 0.2);
-
-    local f = CreateFrame("Frame");
-    f:RegisterEvent("PLAYER_ENTERING_WORLD");
-    f:SetScript("OnEvent", function(self, event)
-        self:UnregisterAllEvents();
-        local scale = numPixelsPerPoint * 768 / yResolution;
-        UIParent:SetScale(scale);
-        A.logi(string.format("pixel-perfect: set: 1 (point) = %d (pixel)", numPixelsPerPoint));
-        A.logi(string.format("pixel-perfect: set: scale = %.06f", scale));
-    end);
-
-    -- not use "percentage" because 100% x 100% is likely a square
-    -- thus introduce dp: the screen height is forced 1024dp
-    local numPointsPerDp = yResolution / numPixelsPerPoint / 1024;
-
-    -- keep 6 digits after the point
+    -- keep 6 digits after the decimal point
     local function round6(number)
         return math.floor(number * 1000000 + 0.5) / 1000000;
     end
 
-    -- Blizzard UI accepts points only. Convert all the units into points.
-    A.px = round6(1 / numPixelsPerPoint);
-    A.dp = round6(numPointsPerDp);
+    local function setup(clientResolution)
+        -- Blizzard UI in unit of points
+        -- canvas has default height of 768 points and is forced to keep aspect ratio
+        -- when scaled:
+        --  canvasHeight in points: 768 / scale
+        --  clientHeight in pixels: clientHeight
+        --  =>  1 (pixel/point) = 768 / scale / clientHeight
+
+        local clientHeight = getClientHeight(clientResolution);
+
+        -- essential! keep a point integer multiple of pixel
+        local numPixelsPerPoint = math.floor(clientHeight / 768 + 0.2);
+        if (numPixelsPerPoint == 0) then
+            numPixelsPerPoint = 1;
+        end
+
+        A.numPixelsPerPoint = numPixelsPerPoint;
+
+        A.scale = numPixelsPerPoint * 768 / clientHeight;
+
+        A.px = round6(1 / numPixelsPerPoint);
+
+        -- introduce dp: the client height is forced 1024dp
+        A.dp = round6(clientHeight / numPixelsPerPoint / 1024);
+    end
+
+    -- not use UiScale cause it must be in [0.64, 1]
+    SetCVar("useUiScale", 0);
+
+    local f = CreateFrame("Frame", nil, nil, nil);
+    f:RegisterEvent("PLAYER_ENTERING_WORLD");
+    f:RegisterEvent("DISPLAY_SIZE_CHANGED");
+    f:SetScript("OnEvent", function(self, event)
+        if (event == "PLAYER_ENTERING_WORLD") then
+            self:UnregisterEvent("PLAYER_ENTERING_WORLD");
+            setup(getSavedClientResolution() or getClientResolution());
+        elseif (event == "DISPLAY_SIZE_CHANGED") then
+            setup(getClientResolution());
+        end
+        UIParent:SetScale(A.scale);
+        A.logi(string.format("set: 1 (point) => [%d] (pixel)", A.numPixelsPerPoint));
+        A.logi(string.format("set: scale => [%.06f]", A.scale));
+    end);
 end)();
