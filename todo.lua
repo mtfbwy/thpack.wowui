185be5a8442cfe024501373878ff4a01b791676e
tanghao <tanghao.hb@gmail.com>
2020-06-16 13:15:30 +0800

    add todo list

---
+++ todo.lua
@@ -0,0 +1,38 @@
+-- mapmark
+--  windowed worldmap
+--  reveal unexplored areas
+--  player and cursor coords
+--  enlarge player arrow
+--  area mob levels
+--  area mine names and levels
+--  area herb names and levels
+--  area fishing levels
+--  poi: dungeon and raid entrances
+--  poi: transportation stations
+--  poi: rare mobs
+--  poi: mailbox
+--  poi: class trainers
+--  poi: profession trainers
+--      RareAware
+--      Townsfolk Tracker
+
+-- unitframe
+--  fix mana regen timer
+--  add cast bar
+--  add hp/mp text on target unitframe
+--  raid ui: gray dot means to far away
+
+-- bag ui
+--  place cover over backpack icon to open/close all bags
+
+-- FlatNameplate
+--  wider health bar
+--  add cast bar
+
+-- SCT
+--  spell name => spell icon
+
+-- player cast bar
+--  reskin: thin bar, text above bar
+
+-- 中国国务院 2013 年发布的《通用规范汉字表》
